22e5d250d963414c93093e9cea862eff88ba432e
Groovyk08 <85116596+Groovyk08@users.noreply.github.com>
2022-10-26 16:26:20 +0200

    projects page update

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -6,7 +6,7 @@ function ShowProjects() {
     return (
         <div>
             <div className="projetsHeader">
-                <h2> Here are my recents projects</h2>
+                <h2> Projects </h2>
             </div>
             <div className="container">
                 <div className="row">
@@ -20,17 +20,22 @@ function ShowProjects() {
                             <div className="cardBody">
                                 <div className="row">
                                     <div className="col">
-                                        <h5>Portfolio</h5>
+                                        <h5>Petits Plats</h5>
                                     </div>
                                     <div className="col">
                                         <div className="techUsed">
                                             <img
-                                                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-                                                alt="React"
+                                                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+                                                alt="HTML"
                                             />
                                             <img
-                                                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
-                                                alt="Bootstrap"
+                                                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+                                                alt="CSS"
+                                            />
+
+                                            <img
+                                                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                                                alt="JAVASCRIPT"
                                             />
                                         </div>
                                     </div>
@@ -39,7 +44,7 @@ function ShowProjects() {
                             <div className="cardFooter">
                                 <div className="cardLink row">
                                     <div className="textCenter col">
-                                        <Link to="https://groovyk08.github.io/Portfolio/">
+                                        <Link to="https://github.com/Groovyk08/PetitsPlats_JS_Project">
                                             Preview on GitHub
                                         </Link>
                                     </div>
